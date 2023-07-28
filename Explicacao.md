108513d907f581f104687632796eaf217033746d
L8K1NH0 <felipewallace489@gmail.com>
2023-07-28 19:43:51 -0300

    Primeira versão do chatPy

---
+++ Explicacao.md
@@ -0,0 +1,13 @@
+##FRONTEND -> Usuario tem acesso / você vê
+    ### html, css, Js
+
+##BACKEND -> Lógica de funcionamento do site
+    ### Python
+
+##FRAMEWORK -> Flask -> Criar Site
+    ###é um conjunto de códigos prontos que servem como estrutura para auxiliar no desenvolvimento
+
+##ambiente virtual -> local no pc com instalações especificas
+    ### criar --> Ctrl + Shift + p -> SELECIONAR(Create Environment) -> .venv
+    
+    ###  Facilita na hora de subir o site para o servidor, econimiza espaço
---
+++ main.py
@@ -0,0 +1,24 @@
+# pip install python-socketio flask-socketio simple-websocket
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send 
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# func de enviar msg
+@socketio.on("message")
+def gerenciar_msg(menssagem):
+    send(menssagem, broadcast=True) # envia msg para todos conectados
+
+# criar primeira pagina = primeira rota
+@app.route("/") # Home page / @ = atribui uma função para oq está em baixo
+def homepage(): # função
+    return render_template("index.html")
+
+# roda o aplicativo
+#app.run(debug=True) # NO FINAL TIRAR MODO DEBUG
+socketio.run(app, host="192.168.1.9") # digitar no terminal (ipconfig), IPv4
+
+# Websocket -> "tunel de conexao" entre PCs
+
